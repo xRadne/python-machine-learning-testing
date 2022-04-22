1d31a708b9d98c88136e1b540fbb16dcb98ff691
xRadne <32938476+xRadne@users.noreply.github.com>
2022-04-22 22:18:04 +0200

    Update formatting

--- Neural network.py
+++ Neural network.py
@@ -1,68 +1,70 @@
 import numpy as np
 
-#sigmoid function
-def nonlin(x, deriv=False): 
-    if(deriv==True):
+
+# Sigmoid function
+def nonlin(x, deriv=False):
+    if(deriv == True):
         return (x*(1-x))
 
     return 1/(1+np.exp(-x))
 
-#input data
-x = np.array([[0,0],
-[0,1],
-[1,0],
-[1,1]])
 
-#target data
+# Input data
+x = np.array([[0, 0],
+              [0, 1],
+              [1, 0],
+              [1, 1]])
+
+# Target data
 y = np.array([[0],
-[1],
-[1],
-[0]])
+              [1],
+              [1],
+              [0]])
 
-#seed (for early debugging)
-#np.random.seed(1)
+# Seed (for early debugging)
+# np.random.seed(1)
 
-#synapses
-syn0 = 2*np.random.random((2,4)) - 1
-syn1 = 2*np.random.random((4,1)) - 1
+# Weights/Synapses
+syn0 = 2*np.random.random((2, 4)) - 1
+syn1 = 2*np.random.random((4, 1)) - 1
 
-#training
+# Training
 print('Training:')
 for j in range(0, 60000):
 
-    #layers
+    # Layers
     l0 = x
     l1 = nonlin(np.dot(l0, syn0))
     l2 = nonlin(np.dot(l1, syn1))
 
-    #backpropagation
+    # Backpropagation
     l2_error = y - l2
     if (j % 10000) == 0:
-        print ('Error: ' + str(np.mean(np.abs(l2_error))))
+        print('Error: ' + str(np.mean(np.abs(l2_error))))
 
-    #calculate deltas
+    # Calculate deltas
     l2_delta = l2_error * nonlin(l2, deriv=True)
     l1_error = l2_delta.dot(syn1.T)
     l1_delta = l1_error * nonlin(l1, deriv=True)
 
-    #update our synapses
+    # Update our synapses
     syn1 += l1.T.dot(l2_delta)
     syn0 += l0.T.dot(l1_delta)
 
-
-#test the model
-print ('\n', 'Output after training')
+# Test the model
+print('\n', 'Output after training')
 for i in range(0, 4):
-    print (x[i][0], ' XOR ', x[i][1], ' => ', l2[i][0])
+    print(x[i][0], ' XOR ', x[i][1], ' => ', l2[i][0])
 
 a = int(2 * np.random.rand(1))
 print("\nTest the model: ")
 num = input("Random number: " + str(a) + ' XOR ')
 
-l0 = np.array([a,int(num)])
+l0 = np.array([a, int(num)])
 l1 = nonlin(np.dot(l0, syn0))
 l2 = nonlin(np.dot(l1, syn1))
 
 print('')
 print('Guess: ' + str(l2[0]))
-print('Round: ' + str(int(np.round(l2[0]))) + ' => ' + str('True' if np.round(l2[0]) == 1 else 'False'))
+print('Round: ' + str(int(np.round(l2[0]))) + ' => ' +
+      str('True' if np.round(l2[0]) == 1 else 'False'))
